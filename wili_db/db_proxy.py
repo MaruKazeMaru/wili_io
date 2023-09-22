d6ec2fe6ded421695f4bc4bf0adc0c6ebd36a9a6
MaruKazeMaru <kaze47111@gmail.com>
2023-09-22 11:08:44 +0900

    update db_proxy.py

--- wili_db/db_proxy.py
+++ wili_db/db_proxy.py
@@ -1,28 +1,38 @@
 import sqlite3
 import rclpy
+from rclpy.parameter import Parameter
 from rclpy.node import Node
-from wili_msgs.srv import GetTrProbMat
+from wili_msgs.msg import HMM
+from wili_msgs.srv import GetHMM
 
 class DBProxy(Node):
     def __init__(self, db_path:str):
         super().__init__("db_proxy")
+
         self.db_conn = sqlite3.connect(db_path)
         self.db_cur = self.db_conn.cursor()
-        self.srv_tr_mat = self.create_service(GetTrProbMat, "get_tr_prob", self.get_tr_prob_mat)
+        self.srv_tr_mat = self.create_service(GetHMM, "get_hmm", self.get_hmm)
 
-    def __del__(self):
+
+    def destroy_node(self):
         self.db_conn.close()
+        super().destroy_node()
 
 
-    def get_tr_prob_mat(self, req:GetTrProbMat.Request, res:GetTrProbMat.Response):
+    def get_hmm(self, req:GetHMM.Request, res:GetHMM.Response):
         self.db_cur.execute('SELECT elem FROM tr_prob ORDER BY from_motion, to_motion')
-        res.data = self.db_cur.fetchall()
+        hmm = HMM()
+        hmm.tr_prob = [f[0] for f in self.db_cur.fetchall()]
+        res.hmm = hmm
         return res
 
 
 def main():
-    node = DBProxy()
+    rclpy.init()
+    node = DBProxy('../test.db')
     rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
