11959bff0833bf20378dc34ea634468762e843ff
MaruKazeMaru <kaze47111@gmail.com>
2023-09-26 00:34:04 +0900

    make db_proxy able to fetch gaussians

--- wili_db/db_proxy.py
+++ wili_db/db_proxy.py
@@ -7,7 +7,7 @@ import sqlite3
 import rclpy
 from rclpy.parameter import Parameter
 from rclpy.node import Node
-from wili_msgs.msg import HMM
+from wili_msgs.msg import HMM, Gaussian, Heatmap
 from wili_msgs.srv import GetHMM
 from math import sqrt
 
@@ -29,17 +29,41 @@ class DBProxy(Node):
 
     def get_hmm(self, req:GetHMM.Request, res:GetHMM.Response):
         self.logger.info('service "/get_hmm" called')
-        self.db_cur.execute('SELECT elem FROM tr_prob ORDER BY from_motion, to_motion')
+
         hmm = HMM()
-        hmm.tr_prob = [f[0] for f in self.db_cur.fetchall()]
-        hmm.motion_num = int(sqrt(len(hmm.tr_prob)))
+
+        # get number of motion
+        self.db_cur.execute('SELECT COUNT(id) FROM motion')
+        hmm.motion_num = self.db_cur.fetchone()
+
+        # get transition probabilities
+        self.db_cur.execute('SELECT elem FROM tr_prob ORDER BY from_motion, to_motion')
+        hmm.tr_prob = [r[0] for r in self.db_cur.fetchall()]
+
+        # get gaussian of each motion
+        self.db_cur.execute('SELCT avr_x, avr_y, var_xx, var_xy, var_yy,  FROM gaussian ORDER BY motion')
+        gs = []
+        for r in self.db_cur.fetchall():
+            g = Gaussian()
+            g.avr_x = r[0]
+            g.avr_y = r[1]
+            g.var_xx = r[2]
+            g.var_xy = r[3]
+            g.var_yy = r[4]
+            gs.append(g)
+
+        for i in range(hmm.motion_num):
+            heatmap = Heatmap()
+            heatmap.gaussian = g[i]
+            hmm.heatmaps.append(heatmap)
+
         res.hmm = hmm
         return res
 
 
 def main():
     rclpy.init()
-    node = DBProxy('../test.db')
+    node = DBProxy('/var/lib/wili/test/db.sqlite3')
     node.logger.info('start')
     try:
         rclpy.spin(node)
